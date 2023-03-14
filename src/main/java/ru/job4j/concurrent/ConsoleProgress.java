3b691e45fc79039d1cf12edf22e96fec0b001d90
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-03-14 16:23:11 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        for (int i = 4; i < 100; i++) {
+            if (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.print("\rLoading.." + process[i % 4]);
+            }
+        }
+    }
+}
