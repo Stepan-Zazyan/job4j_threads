593a71384855e6305b51d13b035edd64d4469b19
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-07-21 10:49:30 +0300

    8. Скачивание файла с ограничением. [#144271 # [#144271]]

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,63 @@
+package ru.job4j.thread;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+
+        var startAt = System.currentTimeMillis();
+        var file = new File("tmp.xml");
+        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        try (var in = new URL(url).openStream();
+             var out = new FileOutputStream(file)) {
+            System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
+            var dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                var downloadAt = System.nanoTime();
+                out.write(dataBuffer, 0, bytesRead);
+                var pause = System.nanoTime() - downloadAt - speed;
+                System.out.println("Read 1024 bytes : " + (pause) + " nano.");
+                if (pause > 0) {
+                    Thread.sleep(pause / 10000);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            System.out.println(Files.size(file.toPath()) + " bytes");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("There is no download limitation time set.");
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
