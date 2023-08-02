49a0832eee7709619980b70aecdf3c68a9e9c7e7
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-08-02 12:02:56 +0300

    3. Денежные переводы AccountStorage [#1104 # [#1104 #432183]]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -1,32 +1,22 @@
 package ru.job4j.cash;
 
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Optional;
 
 public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public synchronized boolean add(Account account) {
-        boolean res = false;
-        if (!accounts.containsValue(account)) {
-            accounts.put(account.id(), account);
-            res = true;
-        }
-        return res;
+        return Objects.equals(account, accounts.putIfAbsent(account.id(), account));
     }
 
     public synchronized boolean update(Account account) {
-        boolean res = false;
-        accounts.put(account.id(), account);
-        if (accounts.get(account.id()).equals(account)) {
-            res = true;
-        }
-        return false;
+        return accounts.replace(account.id(), accounts.get(account.id()), account);
     }
 
     public synchronized boolean delete(int id) {
-        accounts.remove(id);
-        return !accounts.containsKey(id);
+        return Objects.equals(accounts.get(id), accounts.remove(id));
     }
 
     public synchronized Optional<Account> getById(int id) {
@@ -34,15 +24,19 @@ public class AccountStorage {
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
-        int sumFrom = accounts.get(fromId).amount() - amount;
-        if (sumFrom < 0) {
-            throw new IllegalArgumentException("Few money on account");
+        boolean res = false;
+        Optional<Account> accountFrom = getById(fromId);
+        Optional<Account> accountTo = getById(toId);
+        if (accountFrom.isPresent() && accountTo.isPresent()) {
+            int sumFrom = accounts.get(fromId).amount() - amount;
+            if (sumFrom < 0) {
+                throw new IllegalArgumentException("Few money on account");
+            }
+            int sumTo = accounts.get(toId).amount() + amount;
+            update(new Account(fromId, sumFrom));
+            update(new Account(toId, sumTo));
+            res = true;
         }
-        Account accountFrom = new Account(fromId, sumFrom);
-        int sumTo = accounts.get(toId).amount() + amount;
-        Account accountTo = new Account(toId, sumTo);
-        update(accountFrom);
-        update(accountTo);
-        return true;
+        return res;
     }
 }
