eed29f030dff3cfa97db333c9b3046efce9a44ac
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-08-13 08:33:58 +0300

    1. Реализовать ThreadPool [#1099 #436169]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -7,33 +7,45 @@ import java.util.List;
 
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(100);
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
 
     public ThreadPool() {
         int size = Runtime.getRuntime().availableProcessors();
         for (int i = 0; i < size; i++) {
             Thread thread = new Thread(() -> {
                 try {
-                    while (tasks.getQueue().isEmpty()) {
-                        Thread.currentThread().wait();
+                    while (!tasks.getQueue().isEmpty()) {
+                        tasks.poll();
                     }
-                    tasks.poll();
+                    System.out.println("tasks.poll(); " + Thread.currentThread().getName());
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             });
-            this.threads.add(thread);
+            threads.add(thread);
         }
     }
 
     public void work(Runnable job) throws InterruptedException {
-        threads.notifyAll();
-        tasks.offer(job);
+            tasks.offer(job);
+            System.out.println("Did job - offered to tasks " + Thread.currentThread().getName());
     }
 
     public void shutdown() {
         for (Thread x : threads) {
+            System.out.println("Thread Interrupted " + Thread.currentThread().getName());
             x.interrupt();
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool();
+        threadPool.work(() -> System.out.println("working here " + Thread.currentThread().getName()));
+        for (Thread x : threadPool.threads) {
+            x.start();
+            x.join();
+        }
+        threadPool.shutdown();
+        System.out.println(threadPool.threads);
     }
 }
