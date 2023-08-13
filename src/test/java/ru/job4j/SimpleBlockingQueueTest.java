830bed7299b9e9f6ddfae695d6da3e8f673e15a7
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-08-13 08:01:11 +0300

    2. Обеспечить остановку потребителя. [#66825 #436353]

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -42,40 +42,6 @@ class SimpleBlockingQueueTest {
         assertThat(list).isEqualTo(new int[]{1, 2, 3});
         assertThat(list[1]).isEqualTo(2);
     }
-    @Test
-    void checkInALoop() throws InterruptedException {
-        for (int i = 0; i < 10000; i++) {
-            SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(10);
-            int[] list = new int[3];
-            Thread producer = new Thread(
-                    () -> {
-                        try {
-                            sbq.offer(1);
-                            sbq.offer(2);
-                            sbq.offer(3);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-            );
-            Thread consumer = new Thread(
-                    () -> {
-                        try {
-                            list[0] = (sbq.poll());
-                            list[1] = (sbq.poll());
-                            list[2] = (sbq.poll());
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-            );
-            producer.start();
-            producer.join();
-            consumer.start();
-            consumer.join();
-            assertThat(list[1]).isEqualTo(2);
-        }
-    }
 
     @Test
     public void whenFetchAllThenGetIt() throws InterruptedException {
