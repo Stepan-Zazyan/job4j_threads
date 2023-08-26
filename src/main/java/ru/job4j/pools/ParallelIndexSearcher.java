ff860b67a2f1456b9a0ef72bcb39db33fe837081
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-08-26 11:11:21 +0300

    3. ForkJoinPool [#315067 #438130]

--- src/main/java/ru/job4j/pools/ParallelIndexSearcher.java
+++ src/main/java/ru/job4j/pools/ParallelIndexSearcher.java
@@ -2,13 +2,14 @@ package ru.job4j.pools;
 
 import java.util.Objects;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
 public class ParallelIndexSearcher<T> extends RecursiveTask<Integer> {
 
     private T[] array;
-    private static int from;
-    private static int to;
+    private int from;
+    private int to;
     private T res;
 
     public ParallelIndexSearcher(T[] array, int from, int to, T res) {
@@ -24,7 +25,7 @@ public class ParallelIndexSearcher<T> extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
         if (to - from <= 10) {
-            return indexSearch(res, array);
+            return indexSearch();
         }
         int mid = (from + to) / 2;
         ParallelIndexSearcher<T> leftIndex = new ParallelIndexSearcher<>(array, from, mid, res);
@@ -36,20 +37,35 @@ public class ParallelIndexSearcher<T> extends RecursiveTask<Integer> {
         return Math.max(leftJoin, rightJoin);
     }
 
-    public Integer forkIndexSearch(T[] array, ParallelIndexSearcher<T> parallelIndexSearcher) {
+    public static <T> Integer forkIndexSearch(T[] array, T value)  {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        return forkJoinPool.invoke(parallelIndexSearcher);
+        return forkJoinPool.invoke(new ForkJoinTask<>() {
+            @Override
+            public Integer getRawResult() {
+                return null;
+            }
+
+            @Override
+            protected void setRawResult(Integer value) {
+
+            }
+
+            @Override
+            protected boolean exec() {
+                return false;
+            }
+        });
           /*      new ParallelIndexSearcher<>(array, 0, array.length - 1, res);*/
     }
 
-    private static <T> Integer indexSearch(T value, T[] array) {
-        int res = -1;
+    private Integer indexSearch() {
+        int rsl = -1;
         for (int i = from; i < to; i++) {
-            if (Objects.equals(value, array[i])) {
-                res = i;
+            if (Objects.equals(res, array[i])) {
+                rsl = i;
                 break;
             }
         }
-        return res;
+        return rsl;
     }
 }
