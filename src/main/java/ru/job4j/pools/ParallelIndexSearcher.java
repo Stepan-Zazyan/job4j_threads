c902498c9054de23ef3ae81a3433f3f2f8515e4f
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-08-29 09:45:35 +0300

    3. ForkJoinPool [#315067 #438130]

--- src/main/java/ru/job4j/pools/ParallelIndexSearcher.java
+++ src/main/java/ru/job4j/pools/ParallelIndexSearcher.java
@@ -2,24 +2,20 @@ package ru.job4j.pools;
 
 import java.util.Objects;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
 public class ParallelIndexSearcher<T> extends RecursiveTask<Integer> {
 
-    private T[] array;
-    private int from;
-    private int to;
-    private T res;
+    private final T[] array;
+    private final int from;
+    private final int to;
+    private final T obj;
 
-    public ParallelIndexSearcher(T[] array, int from, int to, T res) {
+    public ParallelIndexSearcher(T[] array, int from, int to, T obj) {
         this.array = array;
         this.from = from;
         this.to = to;
-        this.res = res;
-    }
-
-    public ParallelIndexSearcher() {
+        this.obj = obj;
     }
 
     @Override
@@ -28,8 +24,8 @@ public class ParallelIndexSearcher<T> extends RecursiveTask<Integer> {
             return indexSearch();
         }
         int mid = (from + to) / 2;
-        ParallelIndexSearcher<T> leftIndex = new ParallelIndexSearcher<>(array, from, mid, res);
-        ParallelIndexSearcher<T> rightIndex = new ParallelIndexSearcher<>(array, mid + 1, to, res);
+        ParallelIndexSearcher<T> leftIndex = new ParallelIndexSearcher<>(array, from, mid, obj);
+        ParallelIndexSearcher<T> rightIndex = new ParallelIndexSearcher<>(array, mid + 1, to, obj);
         leftIndex.fork();
         rightIndex.fork();
         Integer leftJoin = leftIndex.join();
@@ -37,31 +33,15 @@ public class ParallelIndexSearcher<T> extends RecursiveTask<Integer> {
         return Math.max(leftJoin, rightJoin);
     }
 
-    public static <T> Integer forkIndexSearch(T[] array, T value)  {
+    public static <T> Integer forkIndexSearch(T[] array, T value) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        return forkJoinPool.invoke(new ForkJoinTask<>() {
-            @Override
-            public Integer getRawResult() {
-                return null;
-            }
-
-            @Override
-            protected void setRawResult(Integer value) {
-
-            }
-
-            @Override
-            protected boolean exec() {
-                return false;
-            }
-        });
-          /*      new ParallelIndexSearcher<>(array, 0, array.length - 1, res);*/
+        return forkJoinPool.invoke(new ParallelIndexSearcher<>(array, 0, array.length - 1, value));
     }
 
     private Integer indexSearch() {
         int rsl = -1;
-        for (int i = from; i < to; i++) {
-            if (Objects.equals(res, array[i])) {
+        for (int i = from; i <= to; i++) {
+            if (Objects.equals(obj, array[i])) {
                 rsl = i;
                 break;
             }
--- src/test/java/ru/job4j/pools/ParallelIndexSearcherTest.java
+++ src/test/java/ru/job4j/pools/ParallelIndexSearcherTest.java
@@ -2,39 +2,30 @@ package ru.job4j.pools;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParallelIndexSearcherTest {
-
     @Test
     void searchLinear() {
         Integer[] array = {1, 4, 6, 23, 5, 8};
-        ParallelIndexSearcher<Object> parallelIndexSearcher =
-                new ParallelIndexSearcher<>(array, 0, array.length - 1, 23);
-        Integer a =
-                new ParallelIndexSearcher<>()
-                        .forkIndexSearch(array, parallelIndexSearcher);
-        assertEquals(a, 3);
+        assertEquals(ParallelIndexSearcher.forkIndexSearch(array, 23), 3);
     }
 
     @Test
     void searchParallel() {
         Integer[] array = {1, 4, 6, 23, 5, 8, 4, 4, 5, 0, 6, 7, 8, 45, 3, 2, 4, 9};
-        ParallelIndexSearcher<Object> parallelIndexSearcher =
-                new ParallelIndexSearcher<>(array, 0, array.length - 1, 45);
-        Integer a = new ParallelIndexSearcher<>()
-                        .forkIndexSearch(array, parallelIndexSearcher);
-        assertEquals(13, a);
+        assertEquals(ParallelIndexSearcher.forkIndexSearch(array, 45), 13);
+    }
+
+    @Test
+    void checkFalseSearch() {
+        Integer[] array = {1, 4, 6, 23, 5, 8, 4, 4, 5, 0, 6, 7, 8, 45, 3, 2, 4, 9};
+        assertEquals(ParallelIndexSearcher.forkIndexSearch(array, 645), -1);
     }
 
     @Test
     void checkStringElement() {
         String[] strArray = {"1", "4", "6", "23", "5", "8", "4", "4", "5", "0", "6", "7", "8", "45"};
-        ParallelIndexSearcher<Object> parallelIndexSearcher =
-                new ParallelIndexSearcher<>(strArray, 0, strArray.length - 1, "45");
-        Integer b = new ParallelIndexSearcher<>()
-                        .forkIndexSearch(strArray, parallelIndexSearcher);
-        assertEquals(13, b);
+        assertEquals(ParallelIndexSearcher.forkIndexSearch(strArray, "6"), 10);
     }
 }
